f486b692f36c5723141fa15bc21ea936184f04e0
imgoup <142765693+imgoup@users.noreply.github.com>
2023-08-26 12:37:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 import os
 import requests
+from requests_html import HTMLSession
 app = Flask(__name__)
-
+session = HTMLSession()
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
@@ -11,8 +12,7 @@ def profile(username):
     return f'{username}\'s profile'
 @app.route('/play/<link>')
 def getPlayUrl(link):
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3877.400 QQBrowser/10.8.4506.400'}
-    r = requests.get('https://dlpanda.com/?url=https%3A%2F%2Fv.douyin.com%2FiJGmPD6y%2F&token=G7eRpMaa', headers=headers)
-    return f'{r.status_code}{r.content}'
+    r = session.get('https://dlpanda.com/?url=https%3A%2F%2Fv.douyin.com%2FiJGmPD6y%2F&token=G7eRpMaa')
+    return f'{r.html.find("source", first=True).attrs["src"]}'
     
     
