bf0c862b84de7673c2208047d3e4063ed2c1f281
highSelf <highSelf@users.noreply.github.com>
2023-08-27 00:44:14 +0800

    add local files

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
-from flask import Flask,jsonify,request 
+from flask import Flask, jsonify, request
 import os
 import requests
 from requests_html import HTMLSession
+
 app = Flask(__name__)
 session = HTMLSession()
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+
 @app.route('/user/<username>')
 def profile(username):
     return f'{username}\'s profile'
-@app.route('/play',methods = ['POST', 'GET'])
+
+
+@app.route('/play', methods=['POST', 'GET'])
 def getPlayUrl():
-    playload = { 'url': request.form['link'], 'token': 'G7eRpMaa'}
+    playload = {'url': request.form['link'], 'token': 'G7eRpMaa'}
     r = session.get('https://dlpanda.com', params=playload)
     data = {
         'id': 1343,
         'url': r.html.find("source", first=True).attrs["src"]
     }
     return jsonify(data)
-    
-    
